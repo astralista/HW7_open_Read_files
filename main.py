81dfcf43b2059c3fe5d0cdebb1cd7347a02f4404
Godz Dmitriy <woke@mail.ru>
2022-11-09 17:40:58 +0300

    change main.py

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
 with open('recipes.txt', 'rt') as file:
     cook_book = []
     for line in file:
-        recipe_name = line.strip()
-        ingr_count = int(file.readline())
+        a = line.strip()
+        b = int(file.readline())
         ingridients = []
-    #     for _ in range(ingr_count):
-    #         ingr = file.readline().split(' | ')
-    #         ingredient_name, quantity, measure = ingr
-    #         ingridients.append({'ingredient_name': ingredient_name,
-    #                             'quantity': quantity,
-    #                             'measure': measure
-    #                             })
-    #         file.readline()
-    #         cb = {'name': recipe_name,
-    #          'ingr_count': ingr_count,
-    #          'ingridients': ingridients}
-    #
-    #         cook_book.append(cb)
-        ing = file.readline().split(' | ')
-        ingredient_name, quantity, measure = ing
-    print(ing)
+        for _ in range(b):
+            c = file.readline().split(' | ')
+            ingredient_name, quantity, measure = c
+            ingridients.append({'ingredient_name': ingredient_name,
+                                'quantity': quantity,
+                                'measure': measure
+                                })
+            file.readline()
+            cb = {'name': a,
+             'ingr_count': b,
+             'ingridients': ingridients}
 
+            cook_book.append(cb)
+    # a = file.readline()
+    # b = int(file.readline())
+    # c = file.readline().split(' | ')
+print(cook_book)
 # with open('recipes.txt', 'rt') as file:
 #     content = file.read()
 #     print(content)
