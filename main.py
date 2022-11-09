eb030318e0d3b9c8755e5d7272032c7835b303a8
Godz Dmitriy <woke@mail.ru>
2022-11-09 17:59:03 +0300

    change main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ with open('recipes.txt', 'rt') as file:
         b = int(file.readline())
         ingridients = []
         for _ in range(b):
-            c = file.readline().split(' | ')
+            c = file.readline().strip().split(' | ')
             ingredient_name, quantity, measure = c
             ingridients.append({'ingredient_name': ingredient_name,
                                 'quantity': quantity,
@@ -17,10 +17,27 @@ with open('recipes.txt', 'rt') as file:
              'ingridients': ingridients}
 
             cook_book.append(cb)
-    # a = file.readline()
-    # b = int(file.readline())
-    # c = file.readline().split(' | ')
 print(cook_book)
+# # with open('recipes.txt', 'rt') as file:
+# #     content = file.read()
+# #     print(content)
+
 # with open('recipes.txt', 'rt') as file:
-#     content = file.read()
-#     print(content)
+#     departments = []
+#     for line in file:
+#         department_name = line.strip()
+#         employees_count = int(file.readline())
+#         employees = []
+#         for _ in range(employees_count):
+#             emp = file.readline().strip().split(' | ')
+#             name, last_name, birth_date = emp
+#             employees.append({'name': name,
+#                               'last_name': last_name,
+#                               'birth_date': birth_date})
+#         file.readline()
+#         dep = {'name': department_name,
+#                'employees_count': employees_count,
+#                'employees': employees}
+#         departments.append(dep)
+#
+# print(departments)
