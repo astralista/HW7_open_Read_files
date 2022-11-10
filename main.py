61de2fd89f0cb1bb6c4beef68dcdc79e2305c986
Godz Dmitriy <woke@mail.ru>
2022-11-10 13:46:04 +0300

    task 1 solved

--- main.py
+++ main.py
@@ -1,42 +1,17 @@
-# with open('recipes.txt', 'rt') as file:
-#     cook_book = []
-#     for line in file:
-#         a = line.strip()
-#         b = int(file.readline())
-#         ingridients = []
-#         for _ in range(b):
-#             c = file.readline().strip().split(' | ')
-#             ingredient_name, quantity, measure = c
-#             ingridients.append({'ingredient_name': ingredient_name,
-#                                 'quantity': quantity,
-#                                 'measure': measure})
-#             file.readline()
-#             cb = {'name': a,
-#              'ingr_count': b,
-#              'ingridients': ingridients}
-#
-#             cook_book.append(cb)
-# print(cook_book)
-
-
-with open('recipes.txt', 'rt') as file:
-    departments = []
+with open('recipes.txt', 'r') as file:
+    cook_book = []
     for line in file:
-        department_name = line.strip()
-        employees_count = int(file.readline())
-        employees = []
-        for _ in range(employees_count):
-            emp = file.readline().strip().split(' | ')
-            name, last_name, birth_date = emp
-            employees.append({'name': name,
-                              'last_name': last_name,
-                              'birth_date': birth_date})
+        recipe_name = line.strip()
+        ingr_count = int(file.readline())
+        ingridients = []
+        for _ in range(ingr_count):
+            ingr = file.readline().strip().split(' | ')
+            product, amount, unit = ingr
+            ingridients.append({'ingredient_name': product,
+                                'quantity': amount,
+                                'measure': unit})
         file.readline()
-        dep = {'name': department_name,
-               'employees_count': employees_count,
-               'employees': employees}
-        departments.append(dep)
+        rcps = {recipe_name: ingridients}
+        cook_book.append(rcps)
 
-print(departments)
-
-print('hello')
+print(cook_book)
