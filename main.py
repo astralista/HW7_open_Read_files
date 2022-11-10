663a30d0c08e10a4edc5a58cb2ebe5819fd67f58
Godz Dmitriy <woke@mail.ru>
2022-11-10 17:55:55 +0300

    mf dcts ht allfthm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 with open('recipes.txt', 'r') as file:
-    cook_book = []
+    cook_book = {}
     for line in file:
         recipe_name = line.strip()
         ingr_count = int(file.readline())
@@ -12,6 +12,22 @@ with open('recipes.txt', 'r') as file:
                                 'measure': unit})
         file.readline()
         rcps = {recipe_name: ingridients}
-        cook_book.append(rcps)
+        cook_book.update(rcps)
 
-print(cook_book)
+# print(cook_book)
+#
+#
+#
+def get_shop_list_by_dishes(dishes):
+    dishes_ingr = {}
+    for key in cook_book:
+        if dishes == key:
+            for i in cook_book[key]:
+                for k in i[key]:
+                    print(k)
+    # return dishes_ingr
+
+print(get_shop_list_by_dishes('Омлет'))
+
+# print(cook_book['Омлет'][0])
+
